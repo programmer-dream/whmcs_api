997c5a969fbcb81a48546761e28ad4df0d7e493e
Nick WIlliams <n.a.williams@outlook.com>
2019-02-23 14:02:20 +0000

    d

--- server/app.js
+++ server/app.js
@@ -40,5 +40,8 @@ function validUser(req, res, next) {
 app.get('/home', function(req, res) {
 	res.sendFile('/client/home.html');
 });
-
+/*
 var server = http.createServer(app);
+*/
+var server = app.listen(3000, function(){
+});
