47fafe52d7aa5c5979da80241828255a46006276
Nick WIlliams <n.a.williams@outlook.com>
2019-02-24 19:55:01 +0000

    login and whmcs code

---
+++ server/config/whmcs.js
@@ -0,0 +1,8 @@
+/* var config = {
+    username: 'api_username',
+    password: 'password_as_md5',
+    apiKey: 'zzzzzzzzzzzzzzzzzzzzzzzzzzzzz', // if access without IP restriction
+    serverUrl: 'http://127.0.0.1/includes/api.php'
+  };
+  
+  var wclient = new WHMCS(config); */
