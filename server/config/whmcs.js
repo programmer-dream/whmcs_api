1ab4b6cea54c01b23d78056187e46626878310e7
Nick WIlliams <n.a.williams@outlook.com>
2019-02-25 19:59:26 +0000

    api changes

--- server/config/whmcs.js
+++ server/config/whmcs.js
@@ -1,4 +1,4 @@
-/*const config = {
+const config = {
   identifier: 'uqIfuB386o8vubPz9xTInHp25bQps8EH',
   secret: '2saQiTPgbSIyvUtmSwVBjmqQxPJ58IpR',
   serverUrl: 'http://whmcs.educationhost.co.uk/includes/api.php' // Remember to point to the api.php file
@@ -7,10 +7,10 @@
 
 
 module.exports = config;
-*/
-
+/*
 const config = {
   username: 'nickwilliams99',
   password: 'Password01',
   serverUrl: 'http://whmcs.educationhost.co.uk/includes/api.php' // Remember to point to the api.php file
 }
+*/
