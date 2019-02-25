cef2ebd56c832a2b4b0ace61cb924e74ce0849bc
Nick WIlliams <n.a.williams@outlook.com>
2019-02-25 18:34:27 +0000

    few changes to whmcs config

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const app = express();
 /*--------------------------------------------------------------------------------------------------*/
 
 require('./config/passport.js');
-require('./config/whmcs.js');
+const config = require('./config/whmcs.js');
   
 app.use(passport.initialize());
 app.use(passport.session());
@@ -37,12 +37,6 @@ app.get('/whmcs', function(req, res) {
 });
 
 app.get('/listallwhmcsusers', function(req, res) {
-	var config = {
-    username: 'uqIfuB386o8vubPz9xTInHp25bQps8EH',
-    password: '2saQiTPgbSIyvUtmSwVBjmqQxPJ58IpR',
-    apiKey: 'uqIfuB386o8vubPz9xTInHp25bQps8EH', // if access without IP restriction
-    serverUrl: 'http://whmcs.educationhost.co.uk/includes/api.php'
-	};
 	
 var wclient = new WHMCS(config);
 
--- server/config/whmcs.js
+++ server/config/whmcs.js
@@ -1,9 +1,8 @@
 var config = {
-    username: 'uqIfuB386o8vubPz9xTInHp25bQps8EH',
-    password: '2saQiTPgbSIyvUtmSwVBjmqQxPJ58IpR',
-    apiKey: 'uqIfuB386o8vubPz9xTInHp25bQps8EH', // if access without IP restriction
-    serverUrl: 'http://whmcs.educationhost.co.uk/includes/api.php'
-  };
-  
-// API secret - 2saQiTPgbSIyvUtmSwVBjmqQxPJ58IpR
+  username: 'uqIfuB386o8vubPz9xTInHp25bQps8EH',
+  password: '2saQiTPgbSIyvUtmSwVBjmqQxPJ58IpR',
+  apiKey: 'uqIfuB386o8vubPz9xTInHp25bQps8EH', // if access without IP restriction
+  serverUrl: 'http://whmcs.educationhost.co.uk/includes/api.php'
+};
 
+export default config;
