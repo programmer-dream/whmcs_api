d265447c228aeee6d4d75163137c6287840542c2
Nick WIlliams <n.a.williams@outlook.com>
2019-02-27 21:53:05 +0000

    npm update

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@
 const passport = require('passport');
 const express = require('express');
 var Saml2js = require('saml2js');
-const fs = require('fs-extra');
+const fs = require('fs');
 const app = express();
 
 /*--------------------------------------------------------------------------------------------------*/
