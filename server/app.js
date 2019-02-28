230d74f001dc55e75775dbdaa463702c778d6509
Nick WIlliams <n.a.williams@outlook.com>
2019-02-28 22:20:50 +0000

    keys updated

--- server/app.js
+++ server/app.js
@@ -161,8 +161,8 @@ app.post('/home', function(req, res) {
 var server = http.createServer(app);
 */
 const options = {
-	cert: fs.readFileSync('/home/ehapp/apps/AD-saml/sslcert/app.cer'),
-	key: fs.readFileSync('/home/ehapp/apps/AD-saml/sslcert/app.key')
+	cert: fs.readFileSync('/home/ehapp/apps/AD-saml/sslcert/fullchain.pem'),
+	key: fs.readFileSync('/home/ehapp/apps/AD-saml/sslcert/privkey.pem')
 };
 
 
