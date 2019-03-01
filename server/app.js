d2d4f9b163f094db542c73633c8e2241cc31004b
Nick WIlliams <n.a.williams@outlook.com>
2019-03-01 21:30:10 +0000

    parsedObject

--- server/app.js
+++ server/app.js
@@ -172,12 +172,12 @@ app.post("/login/callback",
 			//console.log(parsedObject);
 					//res.send(parser.asObject());
 
-			//var emailAddress = parser.get('emailAddress');
-			//res.send(emailAddress); //=> 'John'
-			var userId = parser.get('userId');
-			res.send(userId); 
+			var emailAddress = parsedObject.get('emailAddress');
+			res.send(emailAddress); //=> 'John'
 
-					
+
+			userId
+			
 			//var myObj = jQuery.parseJSON(JSON.stringify(parser));
 			//			res.send(myObj);
 
