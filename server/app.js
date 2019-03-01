85908a89486f070a183f3bdccaaba64e9780cb23
Nick WIlliams <n.a.williams@outlook.com>
2019-03-01 18:36:29 +0000

    ghj

--- server/app.js
+++ server/app.js
@@ -205,6 +205,18 @@ function validUser(req, res, next) {
 }
 */
 app.get('/home', function(req, res) {
+
+	var parser = new Saml2js(res.body.SAMLResponse);
+		res.json(parser.asObject());
+		var parsedObject = parser.asObject();
+		var firstName = parser.get('first name');
+		console.log(firstName); //=> 'John'	
+		var firstName = parser.get('email');
+		console.log(firstName); //=> 'John'	
+		var firstName = parser.get('UserID');
+		console.log(firstName); //=> 'John'	
+		var firstName = parser.get('userid');
+		console.log(firstName); //=> 'John'
 	res.sendFile('/home/ehapp/apps/AD-saml/client/home.html');
 });
 
