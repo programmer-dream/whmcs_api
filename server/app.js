f8b6df4a5c8b1bdfaa9e0309911da57ddced9356
Nick WIlliams <n.a.williams@outlook.com>
2019-03-02 14:19:36 +0000

    sdvd

--- server/app.js
+++ server/app.js
@@ -180,7 +180,7 @@ app.post("/login/callback",
 
 		connection.query("SELECT ID FROM user_idpdetails WHERE email = ?",[email], function(err, result, field){
 		//if no result is passed back then the user data should be stored
-			if(!result.email.length){
+			if(!result(email).length){
 					 //new user logic
 					 res.send('New User logic');
 
