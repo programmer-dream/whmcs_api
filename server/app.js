f98fd86f8688ce4ae7cd7c9d41cb39a984270809
Nick WIlliams <n.a.williams@outlook.com>
2019-03-02 15:11:12 +0000

    asd

--- server/app.js
+++ server/app.js
@@ -178,9 +178,9 @@ app.post("/login/callback",
 
 		let connection = mysql.createConnection(config);
 
-		connection.query("SELECT email FROM user_idpdetails WHERE email = ?",[email], function(err, result, field){
+		connection.query("SELECT IFNULL(email,0) FROM user_idpdetails WHERE email = ?",[email], function(err, result, field){
 		//if no result is passed back then the user data should be stored
-			if (!result.length){
+			if (result.length === 0){
 					 //new user logic
 					 //res.send('New User logic');
 
