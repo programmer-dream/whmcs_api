23714ee436724dd49c43b4bf38b9d62375d1c95f
Nick WIlliams <n.a.williams@outlook.com>
2019-03-03 22:29:51 +0000

    gh

--- server/app.js
+++ server/app.js
@@ -230,6 +230,7 @@ app.get('/newusersvariables', function(req, res) {
 		if (err) throw err;
 		newuser.query("SELECT * FROM user_idpdetails WHERE sessionid = ?", [sessionid], function (err, result, fields) {
 			if (err) throw err;
+			newuser.end();	
 			console.log(result);
 			// connection end
 			newuser.end();	
