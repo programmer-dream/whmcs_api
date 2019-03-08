7556919349cb8fe83e8b06165b193c04cdd9e159
Nick WIlliams <n.a.williams@outlook.com>
2019-03-08 21:32:29 +0000

    sdf

--- server/app.js
+++ server/app.js
@@ -249,17 +249,15 @@ app.get("/newusersvariables", function(req, res) {
 
 // The route used to create the student account within WHMCS
 // This takes the data from the signup page and passes it to WHMCS using the WHCMSJS module
-/*
-app.get('/newstudentroute', {
 
-		})
-		.then(function (request) {
-			res.send(data);
-		})
-		.catch(function (error) {
-			console.log(error);
-});
-*/
+app
+  .use("/newstudentroute", {})
+  .then(function(request) {
+    res.send(data);
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
 
 app.get("/home", function(req, res) {
   res.sendFile("/home/ehapp/apps/AD-saml/client/home.html");
