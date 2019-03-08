d18a1c441ef267fc168dda4e37c80ac1cd19d1df
Nick WIlliams <n.a.williams@outlook.com>
2019-03-08 19:58:23 +0000

    sdf

--- server/app.js
+++ server/app.js
@@ -253,7 +253,7 @@ connection.end();
 
 // The route used to create the student account within WHMCS
 // This takes the data from the signup page and passes it to WHMCS using the WHCMSJS module
-
+/*
 app.get('/newstudentroute', {
 
 		})
@@ -263,7 +263,7 @@ app.get('/newstudentroute', {
 		.catch(function (error) {
 			console.log(error);
 });
-
+*/
 
 
 
