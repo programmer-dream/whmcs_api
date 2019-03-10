44f19d854112464e7e13517d244dec38ba35b5c7
Nick WIlliams <n.a.williams@outlook.com>
2019-03-10 17:42:41 +0000

    sdad

--- server/app.js
+++ server/app.js
@@ -358,42 +358,39 @@ app.post('/newstudentroute', (req, res) => {
 
             .then(function (response) {
               console.log(response);
+              // Usernames can be tricky, and because there could be two people with the same name, we need to create a new service username
+              // This will be a random string with the mat.random function
+              const updateClientProduct = new Services(config);
+              const randomstring = Math.random().toString(36).substring(1, 8).toLowerCase().replace(/[\*\^\'\!\.]/g, '').split(' ').join('-');
+              console.log(randomstring);
+              updateClientProduct.updateClientProduct({
+                serviceid: response.productids,
+                serviceusername: randomstring
+              })
+                .then(function (response) {
+                  console.log(response);
 
-            })
-            .catch(function (error) {
-              res.send(error);
-            });
+                  // create the accepted order
+                  const moduleCreate = new Services(config);
 
-          // Usernames can be tricky, and because there could be two people with the same name, we need to create a new service username
-          // This will be a random string with the mat.random function
-          const updateClientProduct = new Services(config);
-          const randomstring = Math.random().toString(36).substring(1, 8).toLowerCase().replace(/[\*\^\'\!\.]/g, '').split(' ').join('-');
-          console.log(randomstring);
-          updateClientProduct.updateClientProduct({
-            serviceid: response.productids,
-            serviceusername: randomstring
-          })
-            .then(function (response) {
-              console.log(response);
-            })
-            .catch(function (error) {
-              res.send(error);
-            });
+                  moduleCreate
+                    .moduleCreate({
 
+                      serviceid: response.productids
 
+                    })
 
-          // create the accepted order
-          const moduleCreate = new Services(config);
+                    .then(function (response) {
+                      console.log(response);
+                    })
+                    .catch(function (error) {
+                      res.send(error);
+                    });
 
-          moduleCreate
-            .moduleCreate({
-
-              serviceid: response.productids
-
-            })
-
-            .then(function (response) {
-              console.log(response);
+                })
+                .catch(function (error) {
+                  res.send(error);
+                });
             })
             .catch(function (error) {
               res.send(error);
@@ -401,6 +398,9 @@ app.post('/newstudentroute', (req, res) => {
 
 
           res.redirect("/");
+
+
+
 
         })
         .catch(function (error) {
