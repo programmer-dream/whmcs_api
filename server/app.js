bc6f3e06dd8d76171762f56b20274d5e98bcdae6
Nick WIlliams <n.a.williams@outlook.com>
2019-03-13 10:15:15 +0000

    sad

--- server/app.js
+++ server/app.js
@@ -235,7 +235,7 @@ app.post("/login/callback", (req, res, next) => {
           [email],
           function (err, result, field) {
             console.log(result);
-            var data = response.data[0];
+            var data = result.data[0];
             if (data.isStaff = 1) {
               res.redirect('/stafflogin')
             } else {
