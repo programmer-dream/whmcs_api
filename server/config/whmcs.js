8ff8f424e673829b71365c3fb2ddb4469d63b0b0
Nick WIlliams <n.a.williams@outlook.com>
2019-03-25 19:56:18 +0000

    changed to EH live

--- server/config/whmcs.js
+++ server/config/whmcs.js
@@ -1,7 +1,15 @@
 const config = {
-  identifier: 'uqIfuB386o8vubPz9xTInHp25bQps8EH',
-  secret: '2saQiTPgbSIyvUtmSwVBjmqQxPJ58IpR',
-  serverUrl: 'http://whmcs.educationhost.co.uk/includes/api.php' // Remember to point to the api.php file
+  /* Test server
+   identifier: 'uqIfuB386o8vubPz9xTInHp25bQps8EH',
+   secret: '2saQiTPgbSIyvUtmSwVBjmqQxPJ58IpR',
+   serverUrl: 'http://whmcs.educationhost.co.uk/includes/api.php' // Remember to point to the api.php file
+ */
+
+  // Pointing to the EH WHMCS Live installation
+  identifier: 'yuYzFn1gQfo7TwUH5L8QezgXKVccbVK1',
+  secret: '7C8ZOvsXteyO4FgbP2C2Z1vAZG8rqvZt',
+  serverUrl: 'http://www.educationhost.co.uk/whmcs/includes/api.php' // Remember to point to the api.php file
+
 }
 
 module.exports = config;
