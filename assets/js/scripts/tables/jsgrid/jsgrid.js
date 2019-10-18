38042749fc3be289c91c8ca3158f71ac9fbe1a95
jordan_edrop@hotmail.co.uk <jordan_edrop@hotmail.co.uk>
2019-10-18 21:22:58 +0100

    Add table filtering

--- assets/js/scripts/tables/jsgrid/jsgrid.js
+++ assets/js/scripts/tables/jsgrid/jsgrid.js
@@ -10,139 +10,247 @@
 
 $(document).ready(function () {
 
-    /****************************
-    *      Basic Scenario       *
-    ****************************/
+	/****************************
+	*      Basic Scenario       *
+	****************************/
 
-    $("#basicScenario").jsGrid({
-        width: "100%",
-        filtering: true,
-        editing: false,
-        inserting: true,
-        sorting: true,
-        paging: true,
-        autoload: true,
-        pageSize: 15,
-        pageButtonCount: 5,
-        controller: db,
-        fields: [
-            { name: "Name", type: "text", width: 100 },
-            {
-                name: "Domain", type: "text",
-                itemTemplate: function (value) {
-                    // return $("<a>").attr("href", value).text(value);
-                    return '<a href="http://' + value + '" target="_blank">' + value + '</a>';
-                }, width: 200, filtering: true
-            },
-            { name: "Module_Year", type: "number", width: 50 },
-            //{ name: "Domain", type: "text", width: 200 },
-            { name: "Module", type: "select", items: db.Modules, valueField: "Id", textField: "Name" },
-            { name: "isActive", type: "checkbox", title: "Is Active", sorting: false },
-            { type: "control" },
-            { name: "fieldname" }
-        ]
-    });
+	$("#basicScenario").jsGrid({
+		width: "100%",
+		filtering: true,
+		editing: false,
+		inserting: true,
+		sorting: true,
+		paging: true,
+		autoload: true,
+		pageSize: 15,
+		pageButtonCount: 5,
+		controller: db,
+		fields: [
+			{ name: "Name", type: "text", width: 100 },
+			{
+				name: "Domain", type: "text",
+				itemTemplate: function (value) {
+					// return $("<a>").attr("href", value).text(value);
+					return '<a href="http://' + value + '" target="_blank">' + value + '</a>';
+				}, width: 200, filtering: true
+			},
+			{ name: "Module_Year", type: "number", width: 50 },
+			//{ name: "Domain", type: "text", width: 200 },
+			{ name: "Module", type: "select", items: db.Modules, valueField: "Id", textField: "Name" },
+			{ name: "isActive", type: "checkbox", title: "Is Active", sorting: false },
+			{ type: "control" },
+			{ name: "fieldname" }
+		]
+	});
 
-    $("#jsGrid").jsGrid({
-        width: "100%",
-        filtering: true,
-        editing: false,
-        inserting: false,
-        sorting: true,
-        paging: true,
-        autoload: true,
-        pageSize: 15,
-        pageButtonCount: 5,
+	$.ajax({
+		type: 'GET',
+		url: '/api/user/staffdashboardgetmodules',
+		dataType: 'json',
+		success: function (modules) {
+			modules.unshift({ value: '' })
 
-        //for loadData method Need to set auto load true
-        autoload: true,
-        pageSize: 50,
-        pageButtonCount: 5,
+			$.ajax({
+				type: 'GET',
+				url: '/api/user/staffdashboardgetyears',
+				dataType: 'json',
+				success: function (years) {
+					years.unshift({ ModuleStartDate: '' })
+					populateGrid(modules, years);
+				},
+				error: function (error) {
+					console.log({ error: error });
+				}
+			});
+		},
+		error: function (jqXHR, exception) {
+			console.log({ error: exception });
+		}
+	});
 
-        noDataContent: "Directory is empty",
+	function contains(element, filters) {
+		return element.toString().toLowerCase().includes(filters.toString().toLowerCase());
+	}
 
-        controller: {
-            loadData: function () {
-                var deferred = $.Deferred();
-                $.ajax({
-                    type: 'GET',
-                    url: '/api/user/staffdashboardlistusers',
-                    dataType: 'json',
-                    success: function (response) {
-                        deferred.resolve(response);
-                    },
-                    error: function (response) {
-                        console.log('error ' + response);
-                    }
-                });
-                return deferred.promise();
-            }
-            /* loadData: function (filter) {
-                 var d = $.Deferred();
+	function isModule(filters, element) {
+		return filters.value === '' || filters.value === element.value;
+	}
+
+	function isUserId(filters, element) {
+		return filters.userid === '' || element.userid == filters.userid;
+	}
+
+	function isFullname(filters, element) {
+		return filters.fullname === '' || contains(element.fullname, filters.fullname);
+	}
+
+	function isUserName(filters, element) {
+		return filters.username === '' || contains(element.username, filters.username);
+	}
+
+	function isDomain(filters, element) {
+		return filters.domain === '' || contains(element.domain, filters.domain);
+	}
+
+	function isDomainModule(filters, element) {
+		return filters.domainmodule === '' || contains(element.domainmodule, filters.domainmodule);
+	}
+
+	function isYears(filters, element) {
+		return filters.ModuleStartDate === '' || element.ModuleStartDate == filters.ModuleStartDate;
+	}
+
+	function filterResults(filters, results) {
+		var filtered = [];
+		for (let i = 0; i < results.length; i++) {
+			const element = results[i];
+			if (
+				isModule(filters, element)
+				&& isUserId(filters, element)
+				&& isFullname(filters, element)
+				&& isUserName(filters, element)
+				&& isDomain(filters, element)
+				&& isDomainModule(filters, element)
+				&& isYears(filters, element)
+			) {
+				filtered.push(element)
+			}
+		}
+		return filtered;
+	}
+
+	function populateGrid(modules, years) {
+		$("#jsGrid").jsGrid({
+			width: "100%",
+			filtering: true,
+			editing: false,
+			inserting: false,
+			sorting: true,
+			paging: true,
+			autoload: true,
+			pageSize: 15,
+			pageButtonCount: 5,
+
+			//for loadData method Need to set auto load true
+			autoload: true,
+			pageSize: 50,
+			pageButtonCount: 5,
+
+			noDataContent: "Directory is empty",
+
+			controller: {
+				loadData: function (filters) {
+					var deferred = $.Deferred();
+					$.ajax({
+						type: 'GET',
+						url: '/api/user/staffdashboardlistusers',
+						dataType: 'json',
+						success: function (response) {
+							var filtered = filterResults(filters, response);
+							deferred.resolve(filtered);
+						},
+						error: function (response) {
+							console.log(response);
+						}
+					});
+					return deferred.promise();
+				}
+				/* loadData: function (filter) {
+						 var d = $.Deferred();
  
-                 // server-side filtering
-                 $.ajax({
-                     type: "GET",
-                     url: "/api/user/staffdashboardlistusers",
-                     data: filter,
-                     dataType: "json"
-                 }).done(function (response) {
-                     // client-side filtering
-                     response = $.grep(response, function (item) {
-                         return item.value === filter.value;
-                     });
+						 // server-side filtering
+						 $.ajax({
+								 type: "GET",
+								 url: "/api/user/staffdashboardlistusers",
+								 data: filter,
+								 dataType: "json"
+						 }).done(function (response) {
+								 // client-side filtering
+								 response = $.grep(response, function (item) {
+										 return item.value === filter.value;
+								 });
  
-                     d.resolve(response);
-                 })
+								 d.resolve(response);
+						 })
  
-                 return d.promise();
-             }*/
-        },
+						 return d.promise();
+				 }*/
+			},
 
-        fields: [{
-            name: "userid",
-            title: 'User ID',
-            type: "text",
-            width: 40
-        }, {
-            name: "fullname",
-            title: 'Name',
-            type: "text"
-        }, {
-            name: "username",
-            title: 'Username',
-            type: "text"
-        }, {
+			fields: [{
+				name: "userid",
+				title: 'User ID',
+				type: "text",
+				width: 40,
+				filtering: true,
+				autosearch: true
+			}, {
+				name: "fullname",
+				title: 'Name',
+				type: "text",
+				filtering: true,
+			}, {
+				name: "username",
+				title: 'Username',
+				type: "text",
+				filtering: true,
+			}, {
 
-            name: "domain", type: "text",
-            title: 'Domain',
-            itemTemplate: function (value) {
-                // return $("<a>").attr("href", value).text(value);
-                return '<a href="http://' + value + '" target="_blank">' + value + '</a>';
-            },
-            width: 180
-        }, {
-            name: "value",
-            title: 'Module',
-            type: "text",
-            filtering: true
-        }, {
+				name: "domain", type: "text",
+				title: 'Domain',
+				itemTemplate: function (value) {
+					// return $("<a>").attr("href", value).text(value);
+					return '<a href="http://' + value + '" target="_blank">' + value + '</a>';
+				},
+				width: 180,
+				filtering: true,
+			}, {
+				name: "value",
+				title: 'Module',
+				type: "select",
+				filtering: true,
+				items: modules,
+				valueField: "value",
+				textField: "value",
+				selectedIndex: -1,
+			}, {
 
-            name: "domainmodule", type: "text",
-            title: 'Module Domain',
-            itemTemplate: function (value) {
-                // return $("<a>").attr("href", value).text(value);
-                return '<a href="http://' + value + '" target="_blank">' + value + '</a>';
-            },
-            width: 180
-        }, {
-            name: "ModuleStartDate",
-            title: 'Module Year',
-            type: "text"
-        }, {
-            type: "control"
-        },]
-    });
+				name: "domainmodule", type: "text",
+				title: 'Module Domain',
+				itemTemplate: function (value) {
+					// return $("<a>").attr("href", value).text(value);
+					return '<a href="http://' + value + '" target="_blank">' + value + '</a>';
+				},
+				width: 180,
+				filtering: true,
+			}, {
+				name: "ModuleStartDate",
+				title: 'Module Year',
+				type: "select",
+				filtering: true,
+				items: years,
+				valueField: "ModuleStartDate",
+				textField: "ModuleStartDate",
+				selectedIndex: -1,
+			}, {
+				type: "control"
+			}
+			]
+		});
 
-    
+		$("#jsGrid :input").keydown(function () {
+			var self = this;
+			if (self.timeout) {
+				clearTimeout(self.timeout);
+			}
+
+			if (self.value.length == 0) {
+				$("#jsGrid").jsGrid('loadData');
+			}
+
+			self.timeout = setTimeout(function () {
+				$("#jsGrid").jsGrid('loadData');
+			}, 500);
+		});
+	}
 });
--- server/routes/api/user.js
+++ server/routes/api/user.js
@@ -16,237 +16,283 @@ const getTimestamp = require("../../utils/getTimestamp");
 // @desc 	Callback for the saml login
 // @access 	Private
 router.post("/login/callback", (req, res, next) => {
-  passport.authenticate(
-    "saml",
-    {
-      session: false
-    },
-    (err, user) => {
-      req.user = user;
-      next();
-    }
-  );
+	passport.authenticate(
+		"saml",
+		{
+			session: false
+		},
+		(err, user) => {
+			req.user = user;
+			next();
+		}
+	);
 
-  var parser = new Saml2js(req.body.SAMLResponse);
+	var parser = new Saml2js(req.body.SAMLResponse);
 
-  const {
-    emailAddress: email,
-    firstName: firstname,
-    userId: userid,
-    lastName: lastname
-  } = parser.toObject();
+	const {
+		emailAddress: email,
+		firstName: firstname,
+		userId: userid,
+		lastName: lastname
+	} = parser.toObject();
 
-  // Check if the received data is valid
-  if (!email || !firstname || !userid || !lastname) {
-    res.redirect("/home");
-    return;
-  }
+	// Check if the received data is valid
+	if (!email || !firstname || !userid || !lastname) {
+		res.redirect("/home");
+		return;
+	}
 
-  req.session.user = {
-    id: userid
-  };
+	req.session.user = {
+		id: userid
+	};
 
-  const sessionid = req.session.id;
+	const sessionid = req.session.id;
 
-  // Decide where the user is going to go, are they new or existing?
-  const connection = mysql.createConnection(mysqlConfig);
+	// Decide where the user is going to go, are they new or existing?
+	const connection = mysql.createConnection(mysqlConfig);
 
-  connection.query(
-    "SELECT email, isStaff FROM user_idpdetails WHERE email = ?",
-    [email],
-    (err, result, field) => {
-      //if no result is passed back then the user data should be stored
-      if (!result.length) {
-        //new user logic
-        /////////////// Store the variables in the db for later use
-        let stmt = `INSERT INTO user_idpdetails(email,firstname,userid,lastname,sessionid) VALUES(?,?,?,?,?)`;
-        let todo = [email, firstname, userid, lastname, sessionid];
+	connection.query(
+		"SELECT email, isStaff FROM user_idpdetails WHERE email = ?",
+		[email],
+		(err, result, field) => {
+			//if no result is passed back then the user data should be stored
+			if (!result.length) {
+				//new user logic
+				/////////////// Store the variables in the db for later use
+				let stmt = `INSERT INTO user_idpdetails(email,firstname,userid,lastname,sessionid) VALUES(?,?,?,?,?)`;
+				let todo = [email, firstname, userid, lastname, sessionid];
 
-        // execute the insert statment
-        connection.query(stmt, todo, (err, results, fields) => {
-          if (err) {
-            return res.send(err.message);
-          } else {
-            // Redirect the user to the home page if the INSERT failed (signup page)
-            res.redirect("/home");
-          }
-        });
+				// execute the insert statment
+				connection.query(stmt, todo, (err, results, fields) => {
+					if (err) {
+						return res.send(err.message);
+					} else {
+						// Redirect the user to the home page if the INSERT failed (signup page)
+						res.redirect("/home");
+					}
+				});
 
-        // close the mysql connection
-        connection.end();
-      } else {
-        // update session id in the database on user login - this is used to pass data from this route to the staff login route
-        connection.query(
-          "UPDATE user_idpdetails SET sessionid = ? WHERE email = ?",
-          [sessionid, email],
-          (error, results, fields) => {
-            if (error) {
-              console.log("error", error);
-            }
-          }
-        );
+				// close the mysql connection
+				connection.end();
+			} else {
+				// update session id in the database on user login - this is used to pass data from this route to the staff login route
+				connection.query(
+					"UPDATE user_idpdetails SET sessionid = ? WHERE email = ?",
+					[sessionid, email],
+					(error, results, fields) => {
+						if (error) {
+							console.log("error", error);
+						}
+					}
+				);
 
-        connection.query(
-          "SELECT * FROM user_idpdetails WHERE email = ?",
-          [email],
-          (err, result, field) => {
-            if (result[0].isStaff == 1 && result[0].isActive == 1) {
-              // res.redirect("/stafflogin");
-              res.redirect("/staff/login");
-            } else if (result[0].isActive == 1) {
-              // get the timestamp in milliseconds and convert it to seconds for WHMCS url
-              var timestamp = getTimestamp();
+				connection.query(
+					"SELECT * FROM user_idpdetails WHERE email = ?",
+					[email],
+					(err, result, field) => {
+						if (result[0].isStaff == 1 && result[0].isActive == 1) {
+							// res.redirect("/stafflogin");
+							res.redirect("/staff/login");
+						} else if (result[0].isActive == 1) {
+							// get the timestamp in milliseconds and convert it to seconds for WHMCS url
+							var timestamp = getTimestamp();
 
-              // get the email address that is returned from the IDP
-              var urlemail = email;
+							// get the email address that is returned from the IDP
+							var urlemail = email;
 
-              // URL to where the user is to go once logged into WHMCS
-              var goto = "clientarea.php";
+							// URL to where the user is to go once logged into WHMCS
+							var goto = "clientarea.php";
 
-              // Auto auth key, this needs to match what is setup in the WHMCS config file (see https://docs.whmcs.com/AutoAuth)
-              // add the three variables together that are required for the WHMCS hash
-              var hashedstrings = urlemail + timestamp + autoAuthKey;
+							// Auto auth key, this needs to match what is setup in the WHMCS config file (see https://docs.whmcs.com/AutoAuth)
+							// add the three variables together that are required for the WHMCS hash
+							var hashedstrings = urlemail + timestamp + autoAuthKey;
 
-              // use the sha1 node module to hash the variable
-              var hash = sha1(hashedstrings);
+							// use the sha1 node module to hash the variable
+							var hash = sha1(hashedstrings);
 
-              // create the URL to pass and redirect the user
-              res.redirect(
-                whmcsLoginUrl +
-                "?email=" +
-                urlemail +
-                "&timestamp=" +
-                timestamp +
-                "&hash=" +
-                hash +
-                "&goto=" +
-                goto
-              );
-            } else {
-              res.redirect("/");
-            }
-          }
-        );
+							// create the URL to pass and redirect the user
+							res.redirect(
+								whmcsLoginUrl +
+								"?email=" +
+								urlemail +
+								"&timestamp=" +
+								timestamp +
+								"&hash=" +
+								hash +
+								"&goto=" +
+								goto
+							);
+						} else {
+							res.redirect("/");
+						}
+					}
+				);
 
-        connection.end();
-      }
-    }
-  );
+				connection.end();
+			}
+		}
+	);
 });
 
 // @route 	GET api/user/
 // @desc 	Get the user variables
 // @access 	Public
 router.get("/", (req, res) => {
-  const sessionid = req.session.id;
-  const connection = mysql.createConnection(mysqlConfig);
+	const sessionid = req.session.id;
+	const connection = mysql.createConnection(mysqlConfig);
 
-  connection.connect(function (err) {
-    if (err) throw err;
+	connection.connect(function (err) {
+		if (err) throw err;
 
-    connection.query(
-      "SELECT * FROM user_idpdetails uidp LEFT JOIN client_details cd ON uidp.universityid = cd.universityid LEFT JOIN client_availablemodules cam ON cd.universityid = cam.universityid WHERE sessionid = ?",
-      [sessionid],
-      (err, result, fields) => {
-        if (err) throw err;
-        res.send(result);
-      }
-    );
+		connection.query(
+			"SELECT * FROM user_idpdetails uidp LEFT JOIN client_details cd ON uidp.universityid = cd.universityid LEFT JOIN client_availablemodules cam ON cd.universityid = cam.universityid WHERE sessionid = ?",
+			[sessionid],
+			(err, result, fields) => {
+				if (err) throw err;
+				res.send(result);
+			}
+		);
 
-    connection.end();
-  });
+		connection.end();
+	});
 });
 
 // @route 	GET api/user/staffdashboardusercount
 // @desc 	Get the user variables
 // @access 	Public
 router.get("/staffdashboardusercount", (req, res) => {
-  const sessionid = req.session.id;
-  const connection = mysql.createConnection(mysqlConfig);
+	const sessionid = req.session.id;
+	const connection = mysql.createConnection(mysqlConfig);
 
-  connection.connect(function (err) {
-    if (err) throw err;
+	connection.connect(function (err) {
+		if (err) throw err;
 
-    connection.query(
-      "SELECT count(ID) AS count FROM user_idpdetails WHERE isActive = 1 && isStaff = 0",
-      [sessionid],
-      (err, result, fields) => {
-        if (err) throw err;
-        res.send(result);
-      }
-    );
+		connection.query(
+			"SELECT count(ID) AS count FROM user_idpdetails WHERE isActive = 1 && isStaff = 0",
+			[sessionid],
+			(err, result, fields) => {
+				if (err) throw err;
+				res.send(result);
+			}
+		);
 
-    connection.end();
-  });
+		connection.end();
+	});
+});
+
+// @route 	GET api/user/staffdashboardgetmodules
+// @desc 	Get the modules
+// @access 	Public
+router.get("/staffdashboardgetmodules", (req, res) => {
+	const sessionid = req.session.id;
+	const whmcsconnection = mysql.createConnection(whmcsmysqlConfig);
+
+	whmcsconnection.connect(function (err) {
+		if (err) throw err;
+
+		whmcsconnection.query(
+			"SELECT DISTINCT(tblcustomfieldsvalues.value) FROM tblclients LEFT JOIN tblhosting ON tblclients.id = tblhosting.userid LEFT JOIN tblcustomfieldsvalues ON tblhosting.userid = tblcustomfieldsvalues.relid LEFT JOIN tblcustomfields ON tblcustomfieldsvalues.fieldid = tblcustomfields.id WHERE tblcustomfieldsvalues.value != '' AND tblcustomfields.type = 'client' AND tblclients.status = 'Active'",
+			[sessionid],
+			(err, result, fields) => {
+				if (err) throw err;
+				res.send(result);
+			}
+		);
+
+		whmcsconnection.end();
+	});
+});
+
+// @route 	GET api/user/staffdashboardgetyears
+// @desc 	Get the years
+// @access 	Public
+router.get("/staffdashboardgetyears", (req, res) => {
+	const sessionid = req.session.id;
+	const whmcsconnection = mysql.createConnection(whmcsmysqlConfig);
+
+	whmcsconnection.connect(function (err) {
+		if (err) throw err;
+
+		whmcsconnection.query(
+			"SELECT DISTINCT YEAR(tblcustomfieldsvalues.created_at) AS ModuleStartDate FROM tblclients LEFT JOIN tblhosting ON tblclients.id = tblhosting.userid LEFT JOIN tblcustomfieldsvalues ON tblhosting.userid = tblcustomfieldsvalues.relid LEFT JOIN tblcustomfields ON tblcustomfieldsvalues.fieldid = tblcustomfields.id WHERE tblcustomfieldsvalues.value != '' AND tblcustomfields.type = 'client' AND tblclients.status = 'Active'",
+			[sessionid],
+			(err, result, fields) => {
+				if (err) throw err;
+				res.send(result);
+			}
+		);
+
+		whmcsconnection.end();
+	});
 });
 
 // @route 	GET api/user/staffdashboardusercount
 // @desc 	Get the user variables
 // @access 	Public
 router.get("/staffdashboardstaffcount", (req, res) => {
-  const sessionid = req.session.id;
-  const connection = mysql.createConnection(mysqlConfig);
+	const sessionid = req.session.id;
+	const connection = mysql.createConnection(mysqlConfig);
 
-  connection.connect(function (err) {
-    if (err) throw err;
+	connection.connect(function (err) {
+		if (err) throw err;
 
-    connection.query(
-      "SELECT count(ID) AS count FROM user_idpdetails WHERE isStaff = 1 && isActive = 1",
-      [sessionid],
-      (err, result, fields) => {
-        if (err) throw err;
-        res.send(result);
-      }
-    );
+		connection.query(
+			"SELECT count(ID) AS count FROM user_idpdetails WHERE isStaff = 1 && isActive = 1",
+			[sessionid],
+			(err, result, fields) => {
+				if (err) throw err;
+				res.send(result);
+			}
+		);
 
-    connection.end();
-  });
+		connection.end();
+	});
 });
 
 // @route 	GET api/user/staffdashboardlistusers
 // @desc 	Get the user variables
 // @access 	Public
 router.get("/staffdashboardlistusers", (req, res) => {
-  const sessionid = req.session.id;
-  const whmcsconnection = mysql.createConnection(whmcsmysqlConfig);
+	const sessionid = req.session.id;
+	const whmcsconnection = mysql.createConnection(whmcsmysqlConfig);
 
-  whmcsconnection.connect(function (err) {
-    if (err) throw err;
+	whmcsconnection.connect(function (err) {
+		if (err) throw err;
 
-    whmcsconnection.query(
-      "SELECT tblclients.id, CONCAT( tblclients.firstname, ' ', tblclients.lastname ) AS fullname, tblclients.email, tblhosting.id, tblhosting.userid, tblhosting.domain, tblhosting.username, tblcustomfields.fieldname, tblcustomfieldsvalues.value, CONCAT( tblhosting.domain, '/', tblcustomfieldsvalues.value ) AS domainmodule, YEAR(tblcustomfieldsvalues.created_at) AS ModuleStartDate FROM tblclients LEFT JOIN tblhosting ON tblclients.id = tblhosting.userid LEFT JOIN tblcustomfieldsvalues ON tblhosting.userid = tblcustomfieldsvalues.relid LEFT JOIN tblcustomfields ON tblcustomfieldsvalues.fieldid = tblcustomfields.id WHERE tblcustomfieldsvalues.value != '' AND tblcustomfields.type = 'client' AND tblclients.status = 'Active'",
-      [sessionid],
-      (err, result, fields) => {
-        if (err) throw err;
-        res.send(result);
-      }
-    );
+		whmcsconnection.query(
+			"SELECT tblclients.id, CONCAT( tblclients.firstname, ' ', tblclients.lastname ) AS fullname, tblclients.email, tblhosting.id, tblhosting.userid, tblhosting.domain, tblhosting.username, tblcustomfields.fieldname, tblcustomfieldsvalues.value, CONCAT( tblhosting.domain, '/', tblcustomfieldsvalues.value ) AS domainmodule, YEAR(tblcustomfieldsvalues.created_at) AS ModuleStartDate FROM tblclients LEFT JOIN tblhosting ON tblclients.id = tblhosting.userid LEFT JOIN tblcustomfieldsvalues ON tblhosting.userid = tblcustomfieldsvalues.relid LEFT JOIN tblcustomfields ON tblcustomfieldsvalues.fieldid = tblcustomfields.id WHERE tblcustomfieldsvalues.value != '' AND tblcustomfields.type = 'client' AND tblclients.status = 'Active'",
+			[sessionid],
+			(err, result, fields) => {
+				if (err) throw err;
+				res.send(result);
+			}
+		);
 
-    whmcsconnection.end();
-  });
+		whmcsconnection.end();
+	});
 });
 
 // @route 	GET api/user/staffdashboardusersupportstats
 // @desc 	Get the user variables
 // @access 	Public
 router.get("/staffdashboardusersupportstats", (req, res) => {
-  const sessionid = req.session.id;
-  const connection = mysql.createConnection(whmcsmysqlConfig);
+	const sessionid = req.session.id;
+	const connection = mysql.createConnection(whmcsmysqlConfig);
 
-  connection.connect(function (err) {
-    if (err) throw err;
+	connection.connect(function (err) {
+		if (err) throw err;
 
-    connection.query(
-      "SELECT AVG(FORMAT(rating,2)) AS Average, COUNT(id) AS NumberOfTickets FROM tblticketfeedback WHERE datetime >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR) && rating != 0",
-      [sessionid],
-      (err, result, fields) => {
-        if (err) throw err;
-        res.send(result);
-      }
-    );
+		connection.query(
+			"SELECT AVG(FORMAT(rating,2)) AS Average, COUNT(id) AS NumberOfTickets FROM tblticketfeedback WHERE datetime >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR) && rating != 0",
+			[sessionid],
+			(err, result, fields) => {
+				if (err) throw err;
+				res.send(result);
+			}
+		);
 
-    connection.end();
-  });
+		connection.end();
+	});
 });
 
 module.exports = router;
