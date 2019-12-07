1d92904202c368df53a0490d47b189b0ccfce4d5
Nadir Abbas <nadirabbasforwork@gmail.com>
2019-12-07 22:42:11 +0500

    Fixes

--- server/routes/view/staff.js
+++ server/routes/view/staff.js
@@ -1,33 +1,81 @@
-const express = require('express');
+const express = require("express");
+
 const router = express.Router();
-const path = require('path');
+
+const path = require("path");
 
 // Gets the client folder which is needed to serve html files
-const root = path.join(__dirname, '../../../../AD-saml/client/staff');
 
-
-
+const root = path.join(__dirname, "../../../../AD-saml/client/staff");
 
 // @route 	GET /staff/dashboard
+
 // @desc 	Serves the staff dashboard
+
 // @access 	Public
-router.get("/dashboard",ensureAuthenticated, function (req, res) {
-	res.render('dashboard',{email:req.user.upn});
+
+router.get("/dashboard", ensureAuthenticated, function(req, res) {
+  res.render("dashboard", {
+    email: req.user.upn,
+    supportMenu: {
+      main: [
+        {
+          label: "Raise Support",
+          url: "http://support.pixinvent.com/",
+          icon: "local_offer"
+        },
+        {
+          label: "FAQ",
+          url: "https://ssotesting.educationhost.co.uk/staff/faq.html",
+          icon: "help_outline"
+        },
+        {
+          label: "Knowledge Base",
+          url:
+            "https://ssotesting.educationhost.co.uk/staff/knowledge-base.html",
+          icon: "info_outline"
+        }
+      ],
+      other: [
+        {
+          label: "Admin Area",
+          url: "https://ssotesting.educationhost.co.uk/staff/news-feed.html",
+          icon: "list_alt"
+        },
+        {
+          label: "Network Monitor",
+          url: "https://uptime.statuscake.com/?TestID=PzBTSXZwia",
+          icon: "list_alt"
+        }
+      ]
+    }
+  });
 });
 
 // @route 	GET /staff/login
-// @desc 	Serves the staff login page
-// @access 	Public
-router.get("/login",ensureAuthenticated1, function (req, res) {
-	res.render("stafflogin");
-});
-function ensureAuthenticated1(req, res, next) {
 
-    if (req.isAuthenticated()) { return next(); }
-    res.redirect('/');
-};
+// @desc 	Serves the staff login page
+
+// @access 	Public
+
+router.get("/login", ensureAuthenticated1, function(req, res) {
+  res.render("stafflogin");
+});
+
+function ensureAuthenticated1(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  res.redirect("/");
+}
+
 function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated() && req.user.isStaff==1) { return next(); }
-    res.redirect('/');
-};
+  if (req.isAuthenticated() && req.user.isStaff == 1) {
+    return next();
+  }
+
+  res.redirect("/");
+}
+
 module.exports = router;
