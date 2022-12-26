f7b0334614bf3e1173355699bbcd8bdc092c217f
pratham <prathammehra50@gmail.com>
2022-12-26 15:55:47 +0530

    module changes

--- models/module_details.js
+++ models/module_details.js
@@ -19,7 +19,7 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.DATE,
       allowNull: false
     },
-    module_end_date: {
+    module_due_date: {
       type: DataTypes.DATE,
       allowNull: false
     }
