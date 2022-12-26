33457c46ff9953aa4e10a38470d7ad9f3c45a535
pratham <prathammehra50@gmail.com>
2022-12-26 20:22:20 +0530

    update status at setting page

--- server/routes/api/staff.js
+++ server/routes/api/staff.js
@@ -759,9 +759,8 @@ let getWhmcsData = (whmcsconnection, queryStr) =>{
 };
 // 26-dec
 router.post("/enableDisable", async (req, res) => {
-    console.log(req.body,"testing");
     let response = await user_idpdetailDal.enabledisablevalue(req.body)
-    console.log(response);
+    res.send({status : 'success', message:'Status updated successfully' })
    
 });
 
